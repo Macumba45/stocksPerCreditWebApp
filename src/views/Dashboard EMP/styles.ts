747f1e32bf066b4f14c8751b9fee7c41695d42c0
Ivan-Oliver <ivanoliverfunes15@gmail.com>
2023-03-21 19:11:09 +0100

    style heart favorites

---
+++ src/views/Dashboard EMP/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MaxContainer = styled.div`
+     margin-left: 100px;
+     margin-top: 2rem;
+     margin-bottom: 3rem;
+     display: flex;
+`
