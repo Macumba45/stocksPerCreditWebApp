ac2495f397ad42995f68c769fb258a0d6ad747bd
Ivan-Oliver <ivanoliverfunes15@gmail.com>
2023-03-21 14:26:26 +0100

    style: empr

--- src/views/Dashboard EMP/index.tsx
+++ src/views/Dashboard EMP/index.tsx
@@ -1,10 +1,16 @@
 import {FC, memo} from 'react';
+import Card from '../../components/CardEmpr';
 import SidebarDashboardEMP from '../../components/SidebarDashboardEMP'
 
 const DashboardEMP: FC = () => {
      return (
           <>
                <SidebarDashboardEMP />
+               <Card image={''} 
+               title={''} description={''} 
+               onClick={function (): void {
+                    throw new Error('Function not implemented.');
+               } } liked={false}/>
           </>
      );
 };
