d160c93ab7588b4301d0b0fa726bd252a24d0f40
Macumba45 <gonzalolovo@gmail.com>
2023-03-21 10:28:20 +0100

    feat: navbar logic

--- src/views/Auth/Signup/index.tsx
+++ src/views/Auth/Signup/index.tsx
@@ -1,140 +1,140 @@
-import {FC, memo, useCallback} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {initialValues} from '../SignUp/constants';
-import {Field, FieldProps, Formik} from 'formik';
-import {validationSchema} from './constants';
-import type {SignUpProps} from './type';
+import { FC, memo, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { initialValues } from '../SignUp/constants';
+import { Field, FieldProps, Formik } from 'formik';
+import { validationSchema } from './constants';
+import type { SignUpProps } from './type';
 import {
-     MainFormContainer,
-     Form,
-     SignUpTitle,
-     EmailContainer,
-     PasswordContainer,
-     LabelContainer,
-     Label,
-     Input,
-     ButtonSignUpContainer,
-     LinkLoginContainer,
-     LinkLoginText,
-     ButtonSignUp,
-     LoginBackImg,
-     Error,
-     NameContainer,
+    MainFormContainer,
+    Form,
+    SignUpTitle,
+    EmailContainer,
+    PasswordContainer,
+    LabelContainer,
+    Label,
+    Input,
+    ButtonSignUpContainer,
+    LinkLoginContainer,
+    LinkLoginText,
+    ButtonSignUp,
+    LoginBackImg,
+    Error,
+    NameContainer,
 } from './styles';
-import {setAuthenticatedToken} from '../../../services/storage';
-import {hadledSubmitSignup} from '../../../services/api/auth';
+import { setAuthenticatedToken } from '../../../services/storage';
+import { hadledSubmitSignup } from '../../../services/api/auth';
 
 const SignUp: FC = () => {
-     const navigate = useNavigate();
+    const navigate = useNavigate();
 
-     const handleSubmit = useCallback(
-          async (values: SignUpProps) => {
-               try {
-                    const response: Response = await hadledSubmitSignup(values);
+    const handleSubmit = useCallback(
+        async (values: SignUpProps) => {
+            try {
+                const response: Response = await hadledSubmitSignup(values);
 
-                    if (response.ok) {
-                         const data = await response.json();
-                         setAuthenticatedToken(data);
-                         navigate('/feed');
-                    }
-               } catch (error: any) {
-                    console.log(error);
-               }
-          },
-          [navigate]
-     );
+                if (response.ok) {
+                     // const data = await response.json();
+                     const data = "12345"
+                    setAuthenticatedToken(data);
+                    navigate('/feed');
+                }
+            } catch (error: any) {
+                console.log(error);
+            }
+        },
+        [navigate]
+    );
 
-     return (
-          <>
-               <LoginBackImg>
-                    <MainFormContainer>
-                         <Formik
-                              validationSchema={validationSchema}
-                              onSubmit={handleSubmit}
-                              initialValues={initialValues}
-                         >
-                              <Form>
-                                   <SignUpTitle>SignUp</SignUpTitle>
-                                   <Field name="name">
-                                        {({field, meta}: FieldProps) => (
-                                             <NameContainer>
-                                                  <LabelContainer>
-                                                       <Label>Name* </Label>
-                                                  </LabelContainer>
-                                                  <Input
-                                                       $hasError={!!meta?.error}
-                                                       type="name"
-                                                       placeholder="Insert your username"
-                                                       autoComplete="email"
-                                                       {...field}
-                                                  />
-                                                  {!!meta?.error && (
-                                                       <Error>
-                                                            {meta.error}
-                                                       </Error>
-                                                  )}
-                                             </NameContainer>
-                                        )}
-                                   </Field>
-                                   <Field name="email">
-                                        {({field, meta}: FieldProps) => (
-                                             <EmailContainer>
-                                                  <LabelContainer>
-                                                       <Label>Email* </Label>
-                                                  </LabelContainer>
-                                                  <Input
-                                                       $hasError={!!meta?.error}
-                                                       type="email"
-                                                       placeholder="Insert your email"
-                                                       autoComplete="email"
-                                                       {...field}
-                                                  />
-                                                  {!!meta?.error && (
-                                                       <Error>
-                                                            {meta.error}
-                                                       </Error>
-                                                  )}
-                                             </EmailContainer>
-                                        )}
-                                   </Field>
-                                   <Field name="password">
-                                        {({field, meta}: FieldProps) => (
-                                             <PasswordContainer>
-                                                  <LabelContainer>
-                                                       <Label>Password* </Label>
-                                                  </LabelContainer>
-                                                  <Input
-                                                       $hasError={!!meta?.error}
-                                                       type="password"
-                                                       autoComplete="current-password"
-                                                       placeholder="Insert password"
-                                                       {...field}
-                                                  />
-                                                  {meta?.error && (
-                                                       <Error>
-                                                            {meta.error}
-                                                       </Error>
-                                                  )}
-                                             </PasswordContainer>
-                                        )}
-                                   </Field>
-                                   <LinkLoginContainer>
-                                        <LinkLoginText to="/login">
-                                             If you are already SignUp, click
-                                             here to login!
-                                        </LinkLoginText>
-                                   </LinkLoginContainer>
-                                   <ButtonSignUpContainer>
-                                        <ButtonSignUp type="submit">
-                                             Sign Up
-                                        </ButtonSignUp>
-                                   </ButtonSignUpContainer>
-                              </Form>
-                         </Formik>
-                    </MainFormContainer>
-               </LoginBackImg>
-          </>
-     );
+    return (
+        <>
+
+            <MainFormContainer>
+                <Formik
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                    initialValues={initialValues}
+                >
+                    <Form>
+                        <SignUpTitle>SignUp</SignUpTitle>
+                        <Field name="name">
+                            {({ field, meta }: FieldProps) => (
+                                <NameContainer>
+                                    <LabelContainer>
+                                        <Label>Name* </Label>
+                                    </LabelContainer>
+                                    <Input
+                                        $hasError={!!meta?.error}
+                                        type="name"
+                                        placeholder="Insert your username"
+                                        autoComplete="email"
+                                        {...field}
+                                    />
+                                    {!!meta?.error && (
+                                        <Error>
+                                            {meta.error}
+                                        </Error>
+                                    )}
+                                </NameContainer>
+                            )}
+                        </Field>
+                        <Field name="email">
+                            {({ field, meta }: FieldProps) => (
+                                <EmailContainer>
+                                    <LabelContainer>
+                                        <Label>Email* </Label>
+                                    </LabelContainer>
+                                    <Input
+                                        $hasError={!!meta?.error}
+                                        type="email"
+                                        placeholder="Insert your email"
+                                        autoComplete="email"
+                                        {...field}
+                                    />
+                                    {!!meta?.error && (
+                                        <Error>
+                                            {meta.error}
+                                        </Error>
+                                    )}
+                                </EmailContainer>
+                            )}
+                        </Field>
+                        <Field name="password">
+                            {({ field, meta }: FieldProps) => (
+                                <PasswordContainer>
+                                    <LabelContainer>
+                                        <Label>Password* </Label>
+                                    </LabelContainer>
+                                    <Input
+                                        $hasError={!!meta?.error}
+                                        type="password"
+                                        autoComplete="current-password"
+                                        placeholder="Insert password"
+                                        {...field}
+                                    />
+                                    {meta?.error && (
+                                        <Error>
+                                            {meta.error}
+                                        </Error>
+                                    )}
+                                </PasswordContainer>
+                            )}
+                        </Field>
+                        <LinkLoginContainer>
+                            <LinkLoginText to="/login">
+                                If you are already SignUp, click
+                                here to login!
+                            </LinkLoginText>
+                        </LinkLoginContainer>
+                        <ButtonSignUpContainer>
+                            <ButtonSignUp type="submit">
+                                Sign Up
+                            </ButtonSignUp>
+                        </ButtonSignUpContainer>
+                    </Form>
+                </Formik>
+            </MainFormContainer>
+        </>
+    );
 };
 
 export default memo(SignUp);
