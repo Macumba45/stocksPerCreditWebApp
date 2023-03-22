89cc89d69937a08219403389ce47de4a81dd7a3b
ManuelRod <manuvski@gmail.com>
2023-03-22 18:10:45 +0100

    refactor:fixing css dashboard

--- src/views/Dashboard INV/styles.ts
+++ src/views/Dashboard INV/styles.ts
@@ -6,7 +6,8 @@ export const Container = styled.div`
      background-color: ${({theme}) => theme.colors.lighterGrey};
 `;
 
-export const MainContainer = styled.div``;
+export const MainContainer = styled.div`
+`;
 
 export const LilDivContainer = styled.div`
      margin-left: ${SIDEBAR_WIDTH};
@@ -15,10 +16,6 @@ export const LilDivContainer = styled.div`
      grid-template-columns: 1fr 1fr 1fr;
      height: 6.25rem;
      @media (max-width: 768px) {
-          /* display: flex;
-          flex-flow: column;
-          margin: auto;
-          height: 45rem; */
           margin-left: 0;
           grid-template-columns: 1fr;
           height: auto;
@@ -29,92 +26,31 @@ export const BigDivsContainer = styled.div`
      display: flex;
      margin: 20px 0 20px ${SIDEBAR_WIDTH};
      & > * {
-          width: 90%;
+          width: 96%;
      }
      @media (max-width: 768px) {
           flex-direction: column;
-          margin: 20px 0;
+          margin-left: 2px;
+          margin-right: 20px;
      }
-;
 `;
 
-export const BigDiv = styled.div``;
+export const BigDiv = styled.div;
 
 export const MediumDiv = styled.div``;
 
-export const MixDivsContainer = styled.div`
-      display: flex;
+export const MixDivsContainer = styled.div` 
+     display: grid;
+     grid-template-columns: 1fr 0.5fr 0.5fr;
      margin: 20px 0 20px ${SIDEBAR_WIDTH};
-     flex-wrap: wrap;
+     margin-right: 20px;
      & > * {
-          width: 90%;
+          width: 96%;
      }
      @media (max-width: 768px) {
-          flex-direction: column;
-          margin: 20px 0;
+          display: block;
+          margin: 20px;
+          margin-right: 20px;
+          margin-left: 5px;
      }
-;
-
-// import styled from 'styled-components';
-
-// export const Container = styled.div`
-//      background-color: ${({theme}) => theme.colors.lighterGrey};
-//      max-width: 100%;`;
-
-// export const MainContainer = styled.div`
-//      /* No se necesita ningún cambio aquí */
-//      @media (max-width: 768px) {
-//           height: 100vh;
-//      }
-// `;
-
-// export const LilDivContainer = styled.div`
-//      margin: 1.5rem 5%;
-//      display: grid;
-//      grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-//      height: 6.25rem;
-
-//      @media (max-width: 768px) {
-//           /* Cambia a dos columnas en pantallas más pequeñas */
-//           /* grid-template-columns: 1fr  */
-//           margin: auto;
-//           width: 22rem;
-//           height: 10rem;
-//           grid-template-columns: 1fr;
-//      }
-// `;
-
-// export const BigDivsContainer = styled.div`
-//      display: flex;
-//      margin: 1.5rem 12%;
-
-//      @media (max-width: 768px) {
-//           /* Cambia el margen en pantallas más pequeñas */
-//           /* margin: 35rem auto; */
-//           /* Cambia a columnas en pantallas más pequeñas */
-//           margin:35rem 0;
-//           display: block;
-//           width: 22rem;
-//           height: 20rem;
-//      }
-// `;
-
-// export const BigDiv = styled.div`
-//      /* No se necesita ningún cambio aquí */
-// `;
-
-// export const MediumDiv = styled.div`
-//      /* No se necesita ningún cambio aquí */
-// `;
-
-// export const MixDivsContainer = styled.div`
-//      display: flex;
-//      margin: 1.5rem 12%;
-
-//      @media (max-width: 768px) {
-//           /* Cambia el margen en pantallas más pequeñas */
-//           margin: 10px 0;
-//           /* Cambia a columnas en pantallas más pequeñas */
-//           flex-direction: column;
-//      }
-// `;
+`;
